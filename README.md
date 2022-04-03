114cad924b1f664eaea163a3bb0d7c1afea5369a
djebby <djebby.firas@gmail.com>
2022-04-03 13:03:53 +0100

    adding Home page

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
+[![MasterHead](./src/assets/images/masterhead.jfif)](https://github.com/djebby)
 <h1 align="center">Hi 👋, I'm Firas Djebby</h1>
-<h3 align="center">Junior Full Stack Developer. :computer:</h3>
+<h2 align="center">Junior Full Stack Developer. :computer:</h2>
+<br>
+<br>
+<p><img align="right" src="./src/assets/images/animation-for-readme.gif" alt="adam-pw" /></p>
+
+- 🌱 I’m currently learning **React Native**
+
+- 💬 Ask me about **ReactJS - NodeJS/ExpressJS - MongoDB - MySQL**
+
+- 📫 How to reach me **djebby.firas@gmail.com**
+
+<br>
+<h3 align="left">Connect with me:</h3>
+<p align="left">
+  <a href="https://www.linkedin.com/in/djebby-firas/" target="blank"><img align="center"
+      src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg"
+      alt="adam pithewan" height="30" width="40" /></a>
+ <a href="https://twitter.com/Firas_Djebby" target="blank"><img align="center"
+      src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg"
+      alt="adampithewan" height="30" width="40" /></a>
+</p>
--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,7 +1,71 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import AnimatedLetters from "../AnimatedLetters";
+import Logo from "../../assets/images/f-logo.png";
+import homeIllustration from "../../assets/images/home-draw.png";
+import "./index.scss";
 
 const Home = () => {
-  return <div className="container">Home</div>;
+  const [letterClass, setLetterClass] = useState("text-animate");
+  const nameArray = ["i", "r", "a", "s"];
+  const jobArray = [
+    "w",
+    "e",
+    "b",
+    " ",
+    "d",
+    "e",
+    "v",
+    "e",
+    "l",
+    "o",
+    "p",
+    "e",
+    "r",
+    ".",
+  ];
+  useEffect(()=>{
+    setTimeout(()=>{
+      setLetterClass("text-animate-hover");
+    }, 4000)
+  }, []);
+  return (
+    <>
+      <div className="container">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img src={Logo} alt="Junior Full Stack Developer Name" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={19}
+            />
+          </h1>
+          <h2>Full Stack Developer / MERN Stack</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <img
+          className="home-illustration"
+          src={homeIllustration}
+          alt="programmer"
+        />
+      </div>
+    </>
+  );
 };
 
 export default Home;
