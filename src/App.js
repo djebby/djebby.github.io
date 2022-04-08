7b7df8bdfec40c54158d58000cd55b67dcc6986a
djebby <djebby.firas@gmail.com>
2022-04-08 10:03:31 +0100

    <HashRouter> insted of <BrowserRouter>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import Layout from "./Components/Layout";
 import Home from "./Components/Home";
@@ -13,6 +13,7 @@ function App() {
         <Route index element={<Home/>}/>
         <Route path="about" element={<About/>}/>
         <Route path="contact" element={<Contact/>}/>
+        <Route path="*" element={<Navigate to="/"/>} />
       </Route>
     </Routes>
   );
